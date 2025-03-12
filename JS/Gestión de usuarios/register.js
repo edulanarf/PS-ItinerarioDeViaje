d826fa16639966e94d7d5f2f94ddb69da07f6f47
edulanarf <edulanarf@gmail.com>
2025-03-12 01:46:24 +0000

    Se añaden funcionalidades como Recuperar contraseña y Correo de Verificación.

--- JS/Gestión de usuarios/register.js
+++ JS/Gestión de usuarios/register.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,6 +28,7 @@ function registerUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential)=> {
       console.log("Usuario Registrado", userCredential.user);
+      sendEmailVerification(userCredential.user);
       alert(`Usuario registrado: ${userCredential.user.email}`);
     })
     .catch((error)=> {
