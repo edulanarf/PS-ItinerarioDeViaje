ac66d3ae90bf810876b7cf2311f658482c7d1de4
edulanarf <edulanarf@gmail.com>
2025-03-13 23:30:35 +0000

    Se corrige la opción "buscar por categorías"

--- JS/Itinerario/Búsqueda de lugares/places.js
+++ JS/Itinerario/Búsqueda de lugares/places.js
@@ -1,138 +1,104 @@
 export const request = {
   "Restaurante": {
     radius: "5000",
-    type: "restaurant"
   },
   "Cafetería": {
     radius: "5000",
-    type: "cafe"
   },
   "Hotel": {
     radius: "7000",
-    type: "hotel"
   },
   "Museo": {
     radius: "5000",
-    type: "museum"
   },
   "Parque": {
     radius: "5000",
-    type: "park"
   },
   "Centro comercial": {
     radius: "7000",
-    type: "shopping_mall"
   },
-  "Aereopuerto": {
-    radius: "20000",
-    type: "airport"
+  "Aeropuerto": {
+    radius: "100000",
   },
   "school": {
     radius: "2000",
-    type: "school"
   },
   "hospital": {
     radius: "2000",
-    type: "hospital"
   },
   "pharmacy": {
     radius: "2000",
-    type: "pharmacy"
   },
   "atm": {
     radius: "2000",
-    type: "atm"
   },
   "bank": {
     radius: "2000",
-    type: "bank"
   },
   "gas_station": {
     radius: "2000",
-    type: "gas_station"
   },
   "store": {
     radius: "2000",
-    type: "store"
   },
   "church": {
     radius: "2000",
-    type: "church"
   },
   "gym": {
     radius: "2000",
-    type: "gym"
   },
   "library": {
     radius: "2000",
-    type: "library"
   },
   "stadium": {
     radius: "2000",
-    type: "stadium"
   },
   "cinema": {
     radius: "2000",
-    type: "cinema"
   },
   "zoo": {
     radius: "2000",
-    type: "zoo"
   },
   "spa": {
     radius: "2000",
-    type: "spa"
   },
   "dentist": {
     radius: "2000",
-    type: "dentist"
   },
   "veterinary_care": {
     radius: "2000",
-    type: "veterinary_care"
   },
   "art_gallery": {
     radius: "2000",
-    type: "art_gallery"
   },
   "tourist_attraction": {
     radius: "2000",
-    type: "tourist_attraction"
   },
   "night_club": {
     radius: "2000",
-    type: "night_club"
   },
   "bus_station": {
     radius: "2000",
-    type: "bus_station"
   },
   "train_station": {
     radius: "2000",
-    type: "train_station"
   },
   "university": {
     radius: "2000",
-    type: "university"
   },
   "post_office": {
     radius: "2000",
-    type: "post_office"
   },
   "casino": {
     radius: "2000",
-    type: "casino"
   },
   "place_of_worship": {
     radius: "2000",
-    type: "place_of_worship"
   },
   "car_rental": {
     radius: "2000",
-    type: "car_rental"
   },
   "lodging": {
     radius: "2000",
-    type: "lodging"
   }
 };
