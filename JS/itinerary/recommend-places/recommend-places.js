579f473458361f90e5ea40272bfcb133e6417ed2
Javier A. Alfonso Quintana <javier.alfonso101@alu.ulpgc.es>
2025-03-24 13:51:31 +0000

    Loads recommended places preferences data

--- JS/itinerary/recommend-places/recommend-places.js
+++ JS/itinerary/recommend-places/recommend-places.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import { request } from '/JS/itinerary/search-places/places.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCpB77wDXu-mNsKKIFg6BddH6DTminG9g",
+  authDomain: "itinerarios-de-viaje-2db0b.firebaseapp.com",
+  projectId: "itinerarios-de-viaje-2db0b",
+  storageBucket: "itinerarios-de-viaje-2db0b.firebasestorage.app",
+  messagingSenderId: "86468425538",
+  appId: "1:86468425538:web:8bc9c4194193614f7cfadb",
+  measurementId: "G-CKN1D6S9GR"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+let service;
+
 window.addEventListener('load',() => {
     document.querySelectorAll('.nav-tabs > a').forEach(el => {
         el.addEventListener('click', e => {
@@ -12,12 +34,67 @@ window.addEventListener('load',() => {
     initMap();
 });
 
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    loadPreferences(user);
+  } else {
+    console.log("No hay usuario autenticado.");
+  }
+});
+
+let preferences;
+
+function loadPreferences(user) {
+  let preferencesRef = doc(db, "preferences", user.uid);
+  getDoc(preferencesRef).then(docSnap => {
+    if (docSnap.exists()) {
+      preferences = docSnap.data();
+    } else {
+      preferences = {
+        cities: [{name:'Madrid',lat:40.416775,lng:-3.703790},{name:'Barcelona',lat:41.390205,lng:2.154007}],
+        categories: ['Restaurante','Cafetería']
+      }
+      //setDoc(preferencesRef,preferences);
+    }
+    loadPreferencesData(preferences).then(preferencesData => {
+      updateSectionsInfo(user,preferences,preferencesData);
+    });
+  })
+}
+
+function loadPreferencesData(preferences) {
+  const promises = [];
+  preferences.cities.forEach(city => {
+    preferences.categories.forEach(category => {
+      const radius = request[category].radius;
+      const location = {lat:city.lat,lng:city.lng};
+      const requests = {
+        location,
+        radius,
+        keyword: category
+      };
+      promises.push(new Promise((resolve,reject)=>{
+        service.nearbySearch(requests, (results, status) => {
+          if (status !== google.maps.places.PlacesServiceStatus.OK) return reject(status);
+          return resolve(results);
+        })
+      }))
+    });
+  });
+  return Promise.all(promises);
+}
+
+function updateSectionsInfo(user,preferences,preferencesData) {
+  
+}
+
 function initMap() {
   const defaultLocation = { lat: 28.1235, lng: -15.4363 }; // Coordenadas de Las Palmas GC
   map = new google.maps.Map(document.getElementById("map"), {
     center: defaultLocation,
     zoom: 12,
   });
+  service = new google.maps.places.PlacesService(map);
 }
 
 // Función para obtener lugares cercanos y mostrarlos
